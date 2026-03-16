e51b82325d86830cab603b5bc22e2746c914c1a3
Zied <shahedzied@gmail.com>
2026-03-16 20:51:23 +0100

    add language screen with header and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'language',
+    pathMatch: 'full'
+  },
+  {
+    path: 'language',
+    loadComponent: () =>
+      import('./features/language/language.component')
+        .then(m => m.LanguageComponent)
+  }
+];
