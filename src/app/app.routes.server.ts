48b4229e9814c18b4c56c3ba0fb4c4661800314f
Zied <shahedzied@gmail.com>
2026-04-12 21:30:26 +0100

    refactorise la structure des composants et ajoute le service de commande

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'splash', renderMode: RenderMode.Prerender },
+  { path: 'language', renderMode: RenderMode.Prerender },
+  { path: 'meat-choice', renderMode: RenderMode.Prerender },
+  { path: 'selection/:type', renderMode: RenderMode.Server },
+  { path: 'cut-type/:type/:part', renderMode: RenderMode.Server },
+  { path: 'quantity-price/:type/:part/:cutType', renderMode: RenderMode.Server },
+  { path: 'quantity-price/:type/:part/:cutType/:size', renderMode: RenderMode.Server },
+  
+  { path: 'emballage/:type/:part/:cutType/:quantity', renderMode: RenderMode.Server },
+  { path: 'emballage/:type/:part/:cutType/:size/:quantity', renderMode: RenderMode.Server },
+
+  { path: '**', renderMode: RenderMode.Server }
 ];
